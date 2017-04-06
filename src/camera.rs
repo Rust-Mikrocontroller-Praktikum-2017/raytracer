285f8b153d2921174b92234e490baed65b49269a
flip1995 <uwdkn@student.kit.edu>
2017-04-06 14:55:14 +0200

    Added camera constructors

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use vector::Vec3;
+use math::{tan, PI};
 
 pub struct Camera {
 
@@ -13,10 +14,43 @@ pub struct Camera {
 
     // distance between camera and image plane
     // (focal length)
-    pub d            :f64,
+    pub focal_dist   :f64,
 
     pub frame_left   :i32,
     pub frame_right  :i32,
     pub frame_bottom :i32,
     pub frame_top    :i32,
+}
+
+const UP: Vec3 = Vec3::new(0.0,0.0,1.0);
+
+impl Camera {
+    pub fn new(pos: &Vec3, target: &Vec3) -> Camera {
+        Camera::new_focal(pos, target, 64.0)
+    }
+    pub fn new_focal(pos: &Vec3, target: &Vec3, focal_dist: f64) -> Camera {
+        Camera::new_focal_fov(pos, target, focal_dist, 90)
+    }
+    pub fn new_focal_fov(pos: &Vec3, target: &Vec3, focal_dist: f64, fov: u8) -> Camera {
+        let fov_rad = ((fov % 180) as f64) * PI / 180.0;
+        let mut w = pos.sub(target);
+        w.normalize();
+        let u = UP.cross(&w);
+        let v = w.cross(&u);
+        let t = (tan((fov/2) as f64) * focal_dist) as i32;
+        let b = -t;
+        let r = 480/272 * t;
+        let l = -r;
+        Camera {
+            pos: *pos,
+            w: w,
+            u: u,
+            v: v,
+            focal_dist: focal_dist,
+            frame_top: t,
+            frame_bottom: b,
+            frame_right: r,
+            frame_left: l,
+        }
+    }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,6 +1,7 @@
 use core::intrinsics;
 
 pub const NAN: f64 = 0.0_f64/0.0_f64;
+pub const PI: f64 = 3.14159265359;
 
 #[inline]
 pub fn sqrt(x : f64) -> f64 {
@@ -9,6 +10,11 @@ pub fn sqrt(x : f64) -> f64 {
     } else {
         unsafe { intrinsics::sqrtf64(x) }
     }
+}
+
+#[inline]
+pub fn tan(x: f64) -> f64 {
+    x + x*x*x/3.0 + 2.0*x*x*x*x*x/15.0
 }
 
 //#[inline]
