ac75100a6eb1bab86ef6fe072774b1e751725772
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-04-06 10:41:31 +0100

    vec2 and vec3 impl

--- src/vector.rs
+++ src/vector.rs
@@ -13,16 +13,6 @@ pub struct Vec2 {
     pub v: f64,
 }
 
-
-impl Vec2 {
-    pub fn new(u :f64, v :f64) -> Vec2 {
-        Vec2 {
-            u: u,
-            v: v,
-        }
-    }
-}
-
 impl Vec3 {
     pub fn new(x :f64, y :f64, z :f64) -> Vec3 {
         Vec3 {
@@ -64,6 +54,30 @@ impl Vec3 {
         self
     }
 
+    pub fn sub(&self, other :&Self) -> &mut Self {
+        Vec3 { 
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+
+    pub fn add(&self, other :&Self) -> &mut Self {
+        Vec3 { 
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+
+    pub fn mult(&self, other :f64) -> &mut Self {
+        Vec3 { 
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+
     pub fn inplace_mult(&mut self, other :f64) -> &mut Self {
         self.x = self.x * other;
         self.y = self.y * other;
@@ -75,17 +89,109 @@ impl Vec3 {
         self.x*other.x + self.y*other.y + self.z*other.z
     }
 
+    pub fn cross(&self, other :&Self) -> f64 {
+        Vec3 {
+            x: self.y*other.z - other.z*self.y,
+            y: self.z*other.x - other.x*self.z,
+            z: self.x*other.y - other.y*self.x,
+        }
+    }
+
     pub fn reflect(&self, n :&Self) -> Self {
         let cos = self.dot(n);
         let mut reflected = n.clone();
 
-
-        //*(reflected.inplace_mult(2.0*cos).inplace_sub(self))
-
-        //let ret = reflected.inplace_mult(2.0*cos).inplace_sub(self);
-        //*ret
-
         reflected.inplace_mult(2.0*cos).inplace_sub(self);
         reflected
+    }
+}
+
+#[test]
+fn can_compute_cross_product() {
+    let vec1 = Vec3::new(1.0,2.0,3.0);
+    let vec2 = Vec3::new(-7.0,8.0,9.0);
+    let res = vec1.cross(vec2);
+
+    assert_eq!(res.x,  -6.0);
+    assert_eq!(res.y, -30.0);
+    assert_eq!(res.z,  22.0);
+}
+
+#[test]
+fn can_reflect_vector() {
+    let mut vec = Vec3::new(-1.0,1.0,0.0);
+    vec.normalize();
+    let mut normal = Vec3::new(0.0,1.0,0.0);
+    normal.normalize();
+
+    let reflected = vec.reflect(&normal);
+
+    assert_eq!(reflected.x, 1.0);
+    assert_eq!(reflected.y, 1.0);
+    assert_eq!(reflected.z, 0.0);
+}
+
+impl Vec2 {
+    pub fn new(u :f64, v :f64) -> Vec2 {
+        Vec2 {
+            u: u,
+            v: v,
+        }
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.u * self.u + self.v * self.v
+    }
+
+    pub fn length(&self) -> f64 {
+        unsafe{intrinsics::sqrtf64(self.length_squared())}
+    }
+
+    pub fn normalize(&mut self) -> &mut Self {
+        let length = self.length();
+
+        self.u = self.u / length;
+        self.v = self.v / length;
+
+        self
+    }
+
+    pub fn inplace_add(&mut self, other :&Self) -> &mut Self {
+        self.u = self.u + other.u;
+        self.v = self.v + other.v;
+        self
+    }
+
+    pub fn inplace_sub(&mut self, other :&Self) -> &mut Self {
+        self.u = self.u - other.u;
+        self.v = self.v - other.v;
+        self
+    }
+
+    pub fn sub(&self, other :&Self) -> &mut Self {
+        Vec3 { 
+            u: self.u - other.u,
+            v: self.v - other.v,
+        }
+    }
+
+    pub fn add(&self, other :&Self) -> &mut Self {
+        Vec3 { 
+            u: self.u + other.u,
+            v: self.v + other.v,
+        }
+    }
+
+    pub fn mult(&self, other :f64) -> &mut Self {
+        Vec3 { 
+            u: self.u * other,
+            v: self.v * other,
+        }
+    }
+
+    pub fn inplace_mult(&mut self, other :f64) -> &mut Self {
+        self.u = self.u * other;
+        self.v = self.v * other;
+        self
     }
 }
