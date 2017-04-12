Rust-Mikrocontroller-Praktikum-2017/raytracer

commit 8eaab712bb34ba5736481ac50e46a72eb5e51e44

Adding pyramid scene

lib/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-#![feature(test, collections, alloc, core_intrinsics)]
+#![feature(collections, alloc, core_intrinsics)]
 
 #![no_std]
 
@@ -70,7 +70,7 @@ fn supersample_bench(b: &mut Bencher, multisample: u32) {
 
         let mut display = NoDisplay {};
 
-        
+
         let mut cam = PerspectiveCamera::new(
             Vec3::new(-400.0,-10.0,500.0),
             Vec3::zero(),

lib/src/intersectables/triangle.rs
@@ -21,7 +21,7 @@ pub struct Triangle<'a> {
 
 impl<'a> Triangle<'a> {
 
-    fn new(a :Vec3, b :Vec3, c :Vec3, material :ModifiedPhongModel) -> Triangle {
+    pub fn new(a :Vec3, b :Vec3, c :Vec3, material :ModifiedPhongModel) -> Triangle {
 
         let vec_ab :Vec3 = Vec3::fromto(&a,&b);
         let vec_ac :Vec3 = Vec3::fromto(&a,&c);

lib/src/scenes/mod.rs
@@ -1,2 +1,3 @@
 pub mod spheres;
 pub mod space;
+pub mod pyramid;

lib/src/scenes/pyramid.rs
@@ -1,0 +1,136 @@
+use scene::Scene;
+use intersectables::sphere::Sphere;
+use intersectables::triangle::Triangle;
+use vector::{Vec3, VEC3_ZERO};
+use reflectionmodel::ModifiedPhongModel;
+use textures::color::{NoTexture};
+use textures::noise::SAND_TEXTURE;
+use texture::MultTexture;
+use texturemapping::sphere::{MapXY};
+
+pub const SCENE_PYRAMID: Scene = Scene {
+    objects: &[
+        &Triangle {
+            a: Vec3 {x: 0.0, y: 0.0, z: 70.0},
+            b: Vec3 {x: -50.0, y: 50.0, z: 0.0},
+            c: Vec3 {x: -50.0, y: -50.0, z: 0.0},
+            vec_ab: Vec3 {x: -50.0, y: 50.0, z: -70.0},
+            vec_ac: Vec3 {x: -50.0, y: -50.0, z: -70.0},
+            normal: Vec3{x: -0.813733459, y: 0.0, z: 0.58123821},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_ambient:      &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Triangle {
+            a: Vec3 {x: 0.0, y: 0.0, z: 70.0},
+            b: Vec3 {x: 50.0, y: 50.0, z: 0.0},
+            c: Vec3{x: -50.0, y: 50.0, z: 0.0},
+            vec_ab: Vec3 {x: 50.0, y: 50.0, z: -70.0},
+            vec_ac: Vec3 {x: -50.0, y: 50.0, z: -70.0},
+            normal: Vec3 {x:0.0, y: 0.813733459, z: 0.58123821},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_ambient:      &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Triangle {
+            a: Vec3 {x: 0.0, y: 0.0, z: 70.0},
+            b: Vec3 {x: 50.0, y: -50.0, z: 0.0},
+            c: Vec3 {x: 50.0, y: 50.0, z: 0.0},
+            vec_ab: Vec3 {x: 50.0, y: -50.0, z: -70.0},
+            vec_ac: Vec3 {x: 50.0, y: 50.0, z: -70.0},
+            normal: Vec3 {x:0.813733459, y: 0.0, z: 0.58123821},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_ambient:      &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Triangle {
+            a: Vec3 {x: 0.0, y: 0.0, z: 70.0},
+            b: Vec3 {x: -50.0, y: -50.0, z: 0.0},
+            c: Vec3 {x: 50.0, y: -50.0, z: 0.0},
+            vec_ab: Vec3 {x: -50.0, y: -50.0, z: -70.0},
+            vec_ac: Vec3 {x: 50.0, y: -50.0, z: -70.0},
+            normal: Vec3{x: 0.0, y: -0.813733459, z: 0.58123821},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_ambient:      &NoTexture { color: Vec3 { x: 0.7, y: 0.7, z: 0.0 } },
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Triangle {
+            a: Vec3 {x: 335.2935, y: 191.404005, z: 0.0},
+            b: Vec3 {x: -23.57535, y: 365.4375, z: 0.0},
+            c: Vec3 {x: -335.2935, y: -147.23385, z: 0.0},
+            vec_ab: Vec3 {x: -358.86885, y: 174.033495, z: 0.0},
+            vec_ac: Vec3 {x: -670.587, y: -338.637855, z: 0.0},
+            normal: Vec3{x: 0.0, y: 0.0, z: 1.0},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &MapXY { texture: &MultTexture { factor: 0.7, texture: &SAND_TEXTURE }},
+                k_ambient:      &MapXY { texture: &MultTexture { factor: 0.2, texture: &SAND_TEXTURE }},
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Triangle {
+            a: Vec3 {x: 335.2935, y: 191.404005, z: 0.0},
+            b: Vec3 {x: -335.2935, y: -147.23385, z: 0.0},
+            c: Vec3 {x: 23.57535, y: -365.4375, z: 0.0},
+            vec_ab: Vec3 {x: -670.587, y: -338.637855, z: 0.0},
+            vec_ac: Vec3 {x: -311.71815, y: -556.841505, z: 0.0},
+            normal: Vec3{x: 0.0, y: 0.0, z: 1.0},
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: VEC3_ZERO },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &MapXY { texture: &MultTexture { factor: 0.7, texture: &SAND_TEXTURE }},
+                k_ambient:      &MapXY { texture: &MultTexture { factor: 0.2, texture: &SAND_TEXTURE }},
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+        &Sphere {
+            center: Vec3 {x: 60.0, y: 100.0, z: 130.0},
+            radius: 0.0,
+            material: ModifiedPhongModel {
+                emission:       &NoTexture { color: Vec3 { x: 2.0e4, y: 2.0e4, z: 2.0e4 } },
+                k_specular:     &NoTexture { color: VEC3_ZERO },
+                k_diffus:       &NoTexture { color: VEC3_ZERO },
+                k_ambient:      &NoTexture { color: VEC3_ZERO },
+                k_t:            &NoTexture { color: VEC3_ZERO },
+
+                phong_exponent: 1.0,
+                ior: 0.0,
+            }
+        },
+    ]
+};

lib/src/scenes/space.rs
@@ -1,13 +1,11 @@
 use scene::Scene;
 use intersectables::sphere::Sphere;
-use intersectables::triangle::Triangle;
 use vector::{Vec3, VEC3_ZERO};
 use reflectionmodel::ModifiedPhongModel;
-use textures::color::{Tiles, NoTexture};
+use textures::color::NoTexture;
 use textures::noise::EARTH_TEXTURE;
-use texturemapping::sphere::{MapXY, MapXZ, MapYZ, Cylindrical, Spherical};
+use texturemapping::sphere::MapXY;
 use texture::MultTexture;
-// use textures::noise::EARTH_TEXTURE;
 
 pub const SCENE_SPACE: Scene = Scene {
     objects: &[

lib/src/scenes/spheres.rs
@@ -1,13 +1,8 @@
 use scene::Scene;
 use intersectables::sphere::Sphere;
-use intersectables::triangle::Triangle;
 use vector::{Vec3, VEC3_ZERO};
 use reflectionmodel::ModifiedPhongModel;
-use textures::color::{Tiles, NoTexture};
-use textures::noise::{LaticeNoise, EARTH_TEXTURE};
-use texturemapping::sphere::{MapXY, MapXZ, MapYZ, Cylindrical, Spherical};
-use texture::MultTexture;
-// use textures::noise::EARTH_TEXTURE;
+use textures::color::{NoTexture};
 
 pub const SCENE_SPHERE: Scene = Scene {
     objects: &[

lib/src/texturemapping/sphere.rs
@@ -1,5 +1,5 @@
 use texture::{Texture, TextureMapping};
-use math::{arccos, atan2, arctan, arcsin, TWOPI, PI};
+use math::{arccos, atan2, arctan, TWOPI, PI};
 use vector::Vec3;
 use intersection::Intersectable;
 
@@ -48,9 +48,9 @@ impl<'a> TextureMapping for MapXY<'a> {
         let objsize = isectable.maximum_expansion(&center) * 2.0;
         let offset = Vec3::fromto(&center, surface_pos);
         let mut s = offset.x / objsize;
-        let s = (1.0 + s) * 0.5;
+        s = (1.0 + s) * 0.5;
         let mut t = offset.y / objsize;
-        let t = (1.0 + t) * 0.5;
+        t = (1.0 + t) * 0.5;
         self.texture.get_texel(s, t)
     }
 }
@@ -65,9 +65,9 @@ impl<'a> TextureMapping for MapXZ<'a> {
         let objsize = isectable.maximum_expansion(&center) * 2.0;
         let offset = Vec3::fromto(&center, surface_pos);
         let mut s = offset.x / objsize;
-        let s = (1.0 + s) * 0.5;
+        s = (1.0 + s) * 0.5;
         let mut t = offset.z / objsize;
-        let t = (1.0 + t) * 0.5;
+        t = (1.0 + t) * 0.5;
         self.texture.get_texel(s, t)
     }
 }
@@ -82,9 +82,9 @@ impl<'a> TextureMapping for MapYZ<'a> {
         let objsize = isectable.maximum_expansion(&center) * 2.0;
         let offset = Vec3::fromto(&center, surface_pos);
         let mut s = offset.y / objsize;
-        let s = (1.0 + s) * 0.5;
+        s = (1.0 + s) * 0.5;
         let mut t = offset.z / objsize;
-        let t = (1.0 + t) * 0.5;
+        t = (1.0 + t) * 0.5;
         self.texture.get_texel(s, t)
     }
 }

lib/src/textures/noise.rs
@@ -67,7 +67,7 @@ const RANDOM :[f32;97]= [
     0.3344491680,0.8642747331,0.5247502835,0.9334799768,0.0000867655,
     0.2837620431,0.0422592508,0.3555656634,0.6729973376,0.5639365988,
     0.1498330680,0.3631261755,//0.3331190519,0.1122175372,0.7005139154,
-];
+    ];
 
 fn reproducable_randomness(u :u16,v :u16, seed :u16) -> f32 {
     // mod using prime numbers to reduce visible
@@ -78,7 +78,8 @@ fn reproducable_randomness(u :u16,v :u16, seed :u16) -> f32 {
 pub struct LaticeNoise {
     pub width  :u16,
     pub height :u16,
-    pub seed   :u16
+    pub seed   :u16,
+    pub color  :Vec3
 }
 
 impl Texture for LaticeNoise {
@@ -87,7 +88,7 @@ impl Texture for LaticeNoise {
         //let vb = max(0.0, min(v, 1.0));
         let ub = modulo(u, 1.0);
         let vb = modulo(v, 1.0);
-        self.color_map(self.get_texel_channel(ub,vb))
+        self.color_map(self.get_texel_channel(ub,vb)).mult_vec(&self.color)
     }
 }
 
@@ -109,7 +110,7 @@ pub struct Turbulence3<'a> {
     width: u16,
     height: u16,
     seed: u16,
-    
+
     octave_1 : LaticeNoise,
     octave_2 : LaticeNoise,
     octave_1_weight :f32,
@@ -158,7 +159,7 @@ impl<'a> LatticeNoiseTexture for Turbulence3<'a> {
 }
 
 //fn u32_to_f32(a :u32) -> f32 {
-    //(a as f32) / (u32::max_value() as f32)
+//(a as f32) / (u32::max_value() as f32)
 //}
 
 fn lerp(a :f32, b :f32, p :f32) -> f32 {
@@ -170,45 +171,56 @@ fn lerp(a :f32, b :f32, p :f32) -> f32 {
 //};
 
 pub const EARTH_TEXTURE :Turbulence3 = Turbulence3 {
-        width:  20,
-        height: 20,
-        seed: 1,
-        
-        octave_1_weight: 0.50,
-        octave_1 : LaticeNoise {
-            width: 40,
-            height: 40,
-            seed: 422,
-        },
+    width:  20,
+    height: 20,
+    seed: 1,
 
-        octave_2_weight: 0.25,
-        octave_2 : LaticeNoise {
-            width: 80,
-            height: 80,
-            seed: 1290,
-        },
+    octave_1_weight: 0.50,
+    octave_1 : LaticeNoise {
+        width: 40,
+        height: 40,
+        seed: 422,
+        color: Vec3 { x: 1.0, y: 1.0, z: 1.0 }
+    },
 
-        color_mapping: &EarthTones {}
+    octave_2_weight: 0.25,
+    octave_2 : LaticeNoise {
+        width: 80,
+        height: 80,
+        seed: 1290,
+        color: Vec3 { x: 1.0, y: 1.0, z: 1.0 }
+    },
+
+    color_mapping: &EarthTones {}
 };
 
 pub const NIGHT_SKY_TEXTURE :Turbulence3 = Turbulence3 {
-        width:  100,
-        height: 100,
-        seed: 1,
-        
-        octave_1_weight: 1.0,
-        octave_1 : LaticeNoise {
-            width: 200,
-            height: 200,
-            seed: 999,
-        },
+    width:  100,
+    height: 100,
+    seed: 1,
 
-        octave_2_weight: 1.0,
-        octave_2 : LaticeNoise {
-            width: 400,
-            height: 400,
-            seed: 9999,
-        },
+    octave_1_weight: 1.0,
+    octave_1 : LaticeNoise {
+        width: 200,
+        height: 200,
+        seed: 999,
+        color: Vec3 { x: 1.0, y: 1.0, z: 1.0 }
+    },
 
-        color_mapping: &SpaceAndStars {}
+    octave_2_weight: 1.0,
+    octave_2 : LaticeNoise {
+        width: 400,
+        height: 400,
+        seed: 9999,
+        color: Vec3 { x: 1.0, y: 1.0, z: 1.0 }
+    },
+
+    color_mapping: &SpaceAndStars {}
+};
+
+pub const SAND_TEXTURE :LaticeNoise = LaticeNoise {
+    width:  500,
+    height: 500,
+    seed: 100,
+    color: Vec3 { x: 1.0, y: 1.0, z: 0.0 }
 };

commit 90659c54acecca9501ec1b6665cec685170a5edc

fix tests, regression in 8eaab71

lib/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![feature(collections, alloc, core_intrinsics)]
 
+#![cfg_attr(test, feature(test))]
+
 #![no_std]
 
 extern crate collections;