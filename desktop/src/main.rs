0809d53e6906af60bad8d654b8a81832fe5b7a94
flip1995 <uwdkn@student.kit.edu>
2017-04-12 15:48:12 +0200

    Fixing line endings again...

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -7,11 +7,8 @@ use rtlib::vector::Vec3;
 use rtlib::render::render;
 use rtlib::camera::Film;
 use rtlib::cameras::perspective::PerspectiveCamera;
-use rtlib::cameras::orthographic::OrthographicCamera;
-use rtlib::scenes::spheres::SCENE_SPHERE;
-use rtlib::textures::color::NoTexture;
+use rtlib::scenes::pyramid::SCENE_PYRAMID;
 use rtlib::textures::file::FileTexture;
-use rtlib::textures::noise::{LaticeNoise, EARTH_TEXTURE, NIGHT_SKY_TEXTURE};
 use display::PngBuffer;
 
 fn main() {
@@ -20,32 +17,30 @@ fn main() {
         y_resolution: 272*3,
         supersampling: 2,
         texture: &FileTexture{
-            width: 1228,
-            height: 613,
-            rgbdata: include_bytes!("../../textures/latlong_palace.rgb")
+            width: 403,
+            height: 161,
+            rgbdata: include_bytes!("../../textures/sky.rgb")
         },
         iso: 100,
     };
 
-
     let mut png = PngBuffer::new(&film);
 
-    
     let mut cam = PerspectiveCamera::new(
-        Vec3::new(-400.0,-10.0,100.0),
-        Vec3::zero(),
+        Vec3::new(-200.0,100.0,150.0),
+        Vec3::new(0.0,0.0,20.0),
         film
     );
 
     cam.set_field_of_view(45);
-    
+
     //let cam = OrthographicCamera::new(
         //Vec3::new(-154.0,0.0,0.0),
         //Vec3::zero(),
         //film
     //);
 
-    render(&mut png, &cam, &SCENE_SPHERE);
+    render(&mut png, &cam, &SCENE_PYRAMID);
 
     png.write();
 }
